b101919f68c183b99753bf16243996f23d4724ca
Kosmas Gourgiotis <kosmasgou@gmail.com>
2024-11-05 14:18:01 +0200

    🚧 stream backend logic impl 🚧

---
+++ lib/azuracast_provider/azuracast_provider.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart';
+import 'package:jbox/models/nowplaying.dart';
+import 'package:http/http.dart' as http;
+
+class AzuracastProvider {
+  static Future<NowPlaying> getNowPlaying({required String url}) async {
+    Future<Response> response = http.get(Uri.parse('$url/api/nowplaying'));
+    NowPlaying nowPlaying = await response.then((value) {
+      return nowPlayingFromJson(value.body)[0];
+    });
+    return nowPlaying;
+  }
+
+  static Stream<NowPlaying> nowPlayingStream({required String url}) async* {
+    // Emit the first NowPlaying data immediately
+    yield await getNowPlaying(url: url);
+
+    // Then, emit data every 5 seconds
+    await for (final _ in Stream.periodic(Duration(seconds: 10))) {
+      yield await getNowPlaying(url: url);
+    }
+  }
+}
--- lib/screens/displayscreen/info_widget.dart
+++ lib/screens/displayscreen/info_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jbox/azuracast_provider/azuracast_provider.dart';
+import 'package:jbox/extensions/debug_print_extension.dart';
+import 'package:jbox/models/nowplaying.dart';
 
 class InfoWidget extends StatelessWidget {
   const InfoWidget({
@@ -12,21 +15,65 @@ class InfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5),
-      decoration: BoxDecoration(color: Colors.black54),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'AZU URL: ${data['azuracastURL'] ?? ''}',
-          ),
-          Text(
-            'terminal name: ${data['displayName'] ?? ''}',
-          ),
-        ],
-      ),
+    double width = MediaQuery.of(context).size.width;
+    'width is ${width}'.printWhite();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        var maxHeight = constraints.maxHeight;
+        var maxWidth = constraints.maxWidth;
+        'maxwidth is ${maxWidth} and maxheight is ${maxHeight}'.printError();
+        var screenTextStyle =
+            TextStyle(fontSize: maxWidth / 40, color: Colors.white);
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(color: Colors.black54),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'AZU URL: ${data['azuracastURL'] ?? ''}',
+                    style: screenTextStyle,
+                  ),
+                  Text(
+                    'terminal name: ${data['displayName'] ?? ''}',
+                    style: screenTextStyle,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(color: Colors.black54),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StreamBuilder<NowPlaying>(
+                      stream: AzuracastProvider.nowPlayingStream(
+                          url: data['azuracastURL']),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                            ),
+                          );
+                        }
+                        return Text(
+                          snapshot.data!.nowPlaying!.elapsed!.toString(),
+                          style: screenTextStyle,
+                        );
+                      })
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
