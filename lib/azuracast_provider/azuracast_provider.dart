1c480a3db23ec7ee1f6d4c996d72b5b92e5dbd09
Kosmas Gourgiotis <kosmasgou@gmail.com>
2024-11-12 20:42:29 +0200

    :bug: big issue with cors fixed with corsproxy :bug:

--- lib/azuracast_provider/azuracast_provider.dart
+++ lib/azuracast_provider/azuracast_provider.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-
 import 'package:http/http.dart';
 import 'package:jbox/extensions/debug_print_extension.dart';
 import 'package:jbox/models/nowplaying.dart';
 import 'package:http/http.dart' as http;
 import 'package:jbox/models/requestlist.dart';
 import 'package:jbox/models/requestsongresponse.dart';
-import 'package:dio/dio.dart' as dioClient;
 
 class AzuracastProvider {
   static Future<NowPlaying> getNowPlaying({required String url}) async {
@@ -81,46 +78,10 @@ class AzuracastProvider {
         .printWhite();
     var urlData =
         'https://corsproxy.io/?$url/api/station/$stationID/request/$requestID';
-    var client = dioClient.Dio();
-    client.interceptors.add(dioClient.InterceptorsWrapper(
-      onRequest: (options, handler) {
-        'interceptor request data -> onSendProgress = ${options.onSendProgress}, path = ${options.path}, options.receiveDataWhenStatusError = ${options.receiveDataWhenStatusError},'
-            .printWarning();
-        handler.next(options);
-      },
-      onResponse: (response, handler) {
-        'interceptor resp data -> data = ${response.data}, statusMessage = ${response.statusMessage}, statusCode = ${response.statusCode},'
-            .printGreen();
-        handler.next(response);
-      },
-      onError: (error, handler) {
-        'interceptor captured resp -> error.response?.data = ${error.response?.data}, error.response?.statusCode = ${error.response?.statusCode}, error.response?.statusMessage = ${error.response?.statusMessage},'
-            .printError();
-        handler.next(error);
-      },
-    ));
-    var responseData;
-    try {
-      responseData = await client.post(
-        data: {},
-        urlData,
-        options: dioClient.Options(
-            followRedirects: false,
-            // will not throw errors
-            validateStatus: (status) => true,
-            headers: {
-              'accept': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
-              'Access-Control-Allow-Headers': '*',
-            }),
-      );
-    } on dioClient.DioException catch (error) {
-      'zzzzzzz error happened so ever zzzzzzz-> ${error.message}'.printError();
-    }
 
-    var requestSongResponse =
-        requestSongResponseFromJson(responseData.toString());
+    var responseData = await http.post(Uri.parse(urlData));
+
+    var requestSongResponse = requestSongResponseFromJson(responseData.body);
     return requestSongResponse;
   }
 }
